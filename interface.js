c53b133690c5c11dcfc65b7c281a07a94fc54e25
Lebranc Convas <lebrancconvas@gmail.com>
2018-04-06 22:07:30 +0700

    Add Interface.

---
+++ interface.js
@@ -0,0 +1,5 @@
+var iam = {
+    name: "poom",
+    age: 20
+};
+console.log(iam.name);
---
+++ interface.ts
@@ -0,0 +1,9 @@
+interface Me {
+  name:string;
+  age:number;
+}
+let iam : Me = {
+  name:"poom",
+  age:20
+}
+console.log(iam.name);
